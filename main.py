f81c93a08daba04a8ad0c9f3b8682bccfff61867
Artem1409 <79360190+Artem1409@users.noreply.github.com>
2023-10-24 22:22:26 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,178 @@
+from tkinter import *
+
+
+def svet1():
+    h.itemconfig(r, outline='blue', width=3)
+    h.itemconfig(y, outline='blue', width=3)
+    h.itemconfig(g, outline='blue', width=2)
+    h.itemconfig(r, fill='red', width=0)
+    h.itemconfig(y, fill='#1c1c08', width=0)
+    h.itemconfig(g, fill='#081c0a', width=0)
+    # root.after(5000, svet2)  # Schedule svet2 after 5 seconds svet6
+    root.after(5000, svet6)
+
+# def svet2():
+#     h.itemconfig(r, outline='blue', width=3)
+#     h.itemconfig(y, outline='blue', width=3)
+#     h.itemconfig(g, outline='blue', width=2)
+#     h.itemconfig(r, fill='black', width=0)
+#     h.itemconfig(y, fill='black', width=0)
+#     h.itemconfig(g, fill='black', width=0)
+#     root.after(500, svet3)  # Schedule svet3 after 5 seconds
+#
+#
+# def svet3():
+#     h.itemconfig(r, outline='blue', width=3)
+#     h.itemconfig(y, outline='blue', width=3)
+#     h.itemconfig(g, outline='blue', width=2)
+#     h.itemconfig(r, fill='red', width=0)
+#     h.itemconfig(y, fill='black', width=0)
+#     h.itemconfig(g, fill='black', width=0)
+#     root.after(500, svet4)  # Schedule svet3 after 5 seconds
+#
+#
+# def svet4():
+#     h.itemconfig(r, outline='blue', width=3)
+#     h.itemconfig(y, outline='blue', width=3)
+#     h.itemconfig(g, outline='blue', width=2)
+#     h.itemconfig(r, fill='black', width=0)
+#     h.itemconfig(y, fill='black', width=0)
+#     h.itemconfig(g, fill='black', width=0)
+#     root.after(500, svet5)  # Schedule svet3 after 5 seconds
+#
+#
+# def svet5():
+#     h.itemconfig(r, outline='blue', width=3)
+#     h.itemconfig(y, outline='blue', width=3)
+#     h.itemconfig(g, outline='blue', width=2)
+#     h.itemconfig(r, fill='red', width=0)
+#     h.itemconfig(y, fill='black', width=0)
+#     h.itemconfig(g, fill='black', width=0)
+#     root.after(500, sveta)  # Schedule svet4 after 5 seconds
+#
+# def sveta():
+#     h.itemconfig(r, outline='blue', width=3)
+#     h.itemconfig(y, outline='blue', width=3)
+#     h.itemconfig(g, outline='blue', width=2)
+#     h.itemconfig(r, fill='black', width=0)
+#     h.itemconfig(y, fill='black', width=0)
+#     h.itemconfig(g, fill='black', width=0)
+#     root.after(500, svetb)  # Schedule svet3 after 5 seconds
+#
+#
+# def svetb():
+#     h.itemconfig(r, outline='blue', width=3)
+#     h.itemconfig(y, outline='blue', width=3)
+#     h.itemconfig(g, outline='blue', width=2)
+#     h.itemconfig(r, fill='red', width=0)
+#     h.itemconfig(y, fill='black', width=0)
+#     h.itemconfig(g, fill='black', width=0)
+#     root.after(500, svet6)  # Schedule svet4 after 5 seconds
+
+
+def svet6():
+    h.itemconfig(r, outline='blue', width=3)
+    h.itemconfig(y, outline='blue', width=3)
+    h.itemconfig(g, outline='blue', width=2)
+    h.itemconfig(r, fill='red', width=0)
+    h.itemconfig(y, fill='yellow', width=0)
+    h.itemconfig(g, fill='#081c0a', width=0)
+    root.after(3000, svet7)  # Schedule svet1 after 5 seconds
+
+
+def svet7():
+    h.itemconfig(r, outline='blue', width=3)
+    h.itemconfig(y, outline='blue', width=3)
+    h.itemconfig(g, outline='blue', width=2)
+    h.itemconfig(r, fill='#1c0808', width=0)
+    h.itemconfig(y, fill='#1c1c08', width=0)
+    h.itemconfig(g, fill='green', width=0)
+    root.after(5000, svet8)  # Schedule svet1 after 5 seconds
+
+
+def svet8():
+    h.itemconfig(r, outline='blue', width=3)
+    h.itemconfig(y, outline='blue', width=3)
+    h.itemconfig(g, outline='blue', width=2)
+    h.itemconfig(r, fill='#1c0808', width=0)
+    h.itemconfig(y, fill='#1c1c08', width=0)
+    h.itemconfig(g, fill='#081c0a', width=0)
+    root.after(500, svets)  # Schedule svet1 after 5 seconds
+
+
+def svets():
+    h.itemconfig(r, outline='blue', width=3)
+    h.itemconfig(y, outline='blue', width=3)
+    h.itemconfig(g, outline='blue', width=2)
+    h.itemconfig(r, fill='#1c0808', width=0)
+    h.itemconfig(y, fill='#1c1c08', width=0)
+    h.itemconfig(g, fill='green', width=0)
+    root.after(500, svetf)  # Schedule svet1 after 5 seconds
+
+
+def svetf():
+    h.itemconfig(r, outline='blue', width=3)
+    h.itemconfig(y, outline='blue', width=3)
+    h.itemconfig(g, outline='blue', width=2)
+    h.itemconfig(r, fill='#1c0808', width=0)
+    h.itemconfig(y, fill='#1c1c08', width=0)
+    h.itemconfig(g, fill='#081c0a', width=0)
+    root.after(500, svetg)  # Schedule svet1 after 5 seconds
+
+
+def svetg():
+    h.itemconfig(r, outline='blue', width=3)
+    h.itemconfig(y, outline='blue', width=3)
+    h.itemconfig(g, outline='blue', width=2)
+    h.itemconfig(r, fill='#1c0808', width=0)
+    h.itemconfig(y, fill='#1c1c08', width=0)
+    h.itemconfig(g, fill='green', width=0)
+    root.after(500, sveth)  # Schedule svet1 after 5 seconds
+
+
+def sveth():
+    h.itemconfig(r, outline='blue', width=3)
+    h.itemconfig(y, outline='blue', width=3)
+    h.itemconfig(g, outline='blue', width=2)
+    h.itemconfig(r, fill='#1c0808', width=0)
+    h.itemconfig(y, fill='#1c1c08', width=0)
+    h.itemconfig(g, fill='#081c0a', width=0)
+    root.after(500, svetk)  # Schedule svet1 after 5 seconds
+
+
+def svetk():
+    h.itemconfig(r, outline='blue', width=3)
+    h.itemconfig(y, outline='blue', width=3)
+    h.itemconfig(g, outline='blue', width=2)
+    h.itemconfig(r, fill='#1c0808', width=0)
+    h.itemconfig(y, fill='#1c1c08', width=0)
+    h.itemconfig(g, fill='green', width=0)
+    root.after(500, svetj)  # Schedule svet1 after 5 seconds
+
+
+def svetj():
+    h.itemconfig(r, outline='blue', width=3)
+    h.itemconfig(y, outline='blue', width=3)
+    h.itemconfig(g, outline='blue', width=2)
+    h.itemconfig(r, fill='#1c0808', width=0)
+    h.itemconfig(y, fill='yellow', width=0)
+    h.itemconfig(g, fill='#081c0a', width=0)
+    root.after(3000, svet1)  # Schedule svet1 after 5 seconds
+
+
+root = Tk()
+
+h = Canvas(width=500, height=350)
+h.pack()
+
+h.create_rectangle(190, 0, 310, 320, fill='black', outline='blue', width=3, dash=(5, 5))
+
+# Adjusted the oval dimensions to make them smaller
+r = h.create_oval(215, 35, 285, 105, fill='red', outline='blue', width=3, dash=(5, 5))
+y = h.create_oval(215, 135, 285, 205, fill="yellow", outline='blue', width=3, dash=(5, 5))
+g = h.create_oval(215, 235, 285, 305, fill='green', outline='blue', width=2, dash=(5, 5))
+
+# Start the automatic light change
+root.after(0, svet1)
+
+root.mainloop()
